665781bd8bc61833e5f44eaa586cbf6c7119c564
WorkNicolas <workcarlnicolasvmendoza@gmail.com>
2023-05-02 07:48:15 +0800

    Create inner class DrawPanel in BinarySearchAnim.java

--- src/mvc/BinarySearchAnim.java
+++ src/mvc/BinarySearchAnim.java
@@ -6,10 +6,17 @@
  */
 package mvc;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
 public class BinarySearchAnim {
     private Model model;
     private View view;
     private Controller ctrl;
+
+    private JPanel panel = new JPanel();
     
     /**
      * MVC arguments are used to ensure bidirectional data
@@ -22,6 +29,32 @@ public class BinarySearchAnim {
         this.model = model;
         this.view = view;
         this.ctrl = ctrl;
-        
+        int[] arr = ctrl.getModel().getArr();
+
+    }
+    /**
+     * Creates boxes with numbers
+     * 
+     */
+    public class DrawPanel extends JPanel {
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            int side = 200;
+            int border = 10;
+            int innerSide = side - 2 * border;
+            int innerX = border;
+            int innerY = border;
+            int number = 5;
+            g.setColor(Color.GREEN);
+            g.fillRect(0, 0, side, side);
+            g.setColor(Color.BLACK);
+            g.drawRect(0, 0, side, side);
+            g.setColor(Color.WHITE);
+            g.fillRect(innerX, innerY, innerSide, innerSide);
+            g.setColor(Color.BLACK);
+            g.drawRect(innerX, innerY, innerSide, innerSide);
+            g.setFont(new Font("Arial", Font.BOLD, 100));
+            g.drawString("" + number, innerX + innerSide/2 - 25, innerY + innerSide/2 + 25);
+        }
     }
 }
