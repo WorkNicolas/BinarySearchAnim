02bf772dadd5c05f98be9db727c0ac8a7d55286d
WorkNicolas <workcarlnicolasvmendoza@gmail.com>
2023-05-01 16:12:33 +0800

    Added MVC arch to BinarySearchAnim

--- src/mvc/BinarySearchAnim.java
+++ src/mvc/BinarySearchAnim.java
@@ -7,5 +7,20 @@
 package mvc;
 
 public class BinarySearchAnim {
-    
+    private Model model;
+    private View view;
+    private Controller ctrl;
+    /**
+     * MVC arguments are used to ensure bidirectional data
+     * 
+     * @param model Model architecture
+     * @param view View Architecture
+     * @param ctrl Controller Architecture
+     */
+    public BinarySearchAnim (Model model, View view, Controller ctrl) {
+        this.model = model;
+        this.view = view;
+        this.ctrl = ctrl;
+        
+    }
 }
